e24c35a2fa1df6c053e1577904df1ba5e76409c8
Olawale Shopeyin <shopeyinwale@gmail.com>
2023-03-12 10:25:16 +0100

    Banner component

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,20 @@
+import "./Banner.scss";
+import BannerImg from "../../assets/banner.png";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h1 className="title">SUI AIRDROP - WHY YOU SHOULD GET IT!</h1>
+      <p>Olawale J. Shopeyin</p>
+      <div className="container">
+        <img src={BannerImg} alt="" className="banner-img" />
+        <h2 className="description">
+          AN IN-DEPTH ANALYSIS OF THE SUI ECOSYSTEM AND ITS AIRDROP
+        </h2>
+        <p>Research piece</p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,31 +2,38 @@ import React from "react";
 import "./Home.scss";
 import Headline from "../../assets/headline.png";
 import Preview from "../../components/Preview/Preview";
+import Banner from "../../components/Banner/Banner";
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="left-grid">
-        <img src={Headline} alt="" className="headline" />
-        <div className="heading-container">
-          <h1 className="heading">
-            TALKING NFTS, <br /> CRYPTO AND <br /> DECENTRALIZED FINANCE <br />
-            IN THIS BEAR RUN
-          </h1>
-          <p className="author">Michael Shopeyin</p>
-          <p className="title">
-            Understanding what makes a Bear run tick, and how to get the best of
-            the run, because you can....
-          </p>
+    <>
+      <div className="home">
+        <div className="left-grid">
+          <img src={Headline} alt="" className="headline" />
+          <div className="heading-container">
+            <h1 className="heading">
+              TALKING NFTS, <br /> CRYPTO AND <br /> DECENTRALIZED FINANCE{" "}
+              <br />
+              IN THIS BEAR RUN
+            </h1>
+            <p className="author">Michael Shopeyin</p>
+            <p className="title">
+              Understanding what makes a Bear run tick, and how to get the best
+              of the run, because you can....
+            </p>
+          </div>
+        </div>
+        <div className="right-grid">
+          <Preview />
+          <Preview />
+          <Preview />
+          <Preview />
         </div>
       </div>
-      <div className="right-grid">
-        <Preview />
-        <Preview />
-        <Preview />
-        <Preview />
+      <div className="banner-overview">
+        <Banner />
       </div>
-    </div>
+    </>
   );
 };
 
