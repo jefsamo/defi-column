be2f187e7ba52e8ed7304e25913dfe7cd73b0009
Olawale Shopeyin <shopeyinwale@gmail.com>
2023-03-12 05:32:10 +0100

    Project initialization

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="app">Defi column</div>;
+}
+
+export default App;
