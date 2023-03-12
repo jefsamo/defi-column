542c4198e578eeb17fe4a11bacdb2465534cbbb4
Olawale Shopeyin <shopeyinwale@gmail.com>
2023-03-12 06:52:53 +0100

    Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="app">Defi column</div>;
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <div>Home page</div>,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+import { RxHamburgerMenu } from "react-icons/rx";
+import { Link } from "react-router-dom";
+import D from "../../assets/D.png";
+import Column from "../../assets/column.png";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="inner-navbar">
+        <div className="hamburger">
+          <RxHamburgerMenu size={30} className="ham-menu" />
+          <div className="nav-links">
+            <li>
+              <Link to="/">Defi</Link>
+            </li>
+            <li>
+              <Link to="/">NFTs</Link>
+            </li>
+            <li>
+              <Link to="/">Bullrun</Link>
+            </li>
+            <li>
+              <Link to="/">Writers</Link>
+            </li>
+          </div>
+        </div>
+        <div className="logo">
+          <img src={D} alt="d-logo" />
+          <img src={Column} alt="column-logo" className="column" />
+        </div>
+        <div className="subscribe">
+          <li>
+            <Link to="/">Subscribe</Link>
+          </li>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
